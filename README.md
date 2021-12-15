7c7a890d2b4a400d0f12a172e64a14be955d1db1
I'm NK <isNK@1.local>
2021-12-15 23:37:47 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # IELTS
 leanring english 
+
+This is an IELTS practise system,For future.
+
+# How use this it 
+
+just run ``python main.py``.
---
+++ src/xwg.py
@@ -0,0 +1,40 @@
+# encoding:utf-8
+# create date: 2021-12-14 
+# by NK
+# Book: 学为贵538考点词
+class system:
+        BOOK_NAME = "学为贵538考点词"
+        BOOK_TAG = "xwg538"
+        def __init__(self):
+                pass
+        def bookWordsFirst(self):
+                words = (
+                        ["resemble",r"像，与……相似","like,look like,be similar to"],
+                        ["recognize",r"认出，识别；承认","perceive,acknowledge,realize,appreciate,admit,identify,comprehend,understand,know"],
+                        ["adjust",r"调整，使合适","change,modify,shift,alter"],
+                        ["approach",r"方法","method,way"],
+                        ["fundamental",r"基本的，基础的","rudimentary,preliminary,basic"],
+                        ["rely on",r"依赖，依靠","depend on"],
+                        ["domestic",r"家庭的，国内的","home,local,national"],
+                        ["measure",r"测量","calculate,assessm,evaluate"],
+                        ["trait",r"特性，特征","characteristic,feature,property"],
+                        ["coin",r"创造，硬币","first used, invent"],
+                        ["artificial",r"人造的，仿造的","synthetic,man-made"],
+                        ["prompt",r"促进，激起","initiate，immediately"],
+                        ["exchange",r"交换","share,apply A to B"],
+                        ["underlie",r"成为什么的基础","based on,ground,root"],
+                        ["ignore",r"忽视，不顾","neglect,overlook,underestimate"],
+                        ["fertiliser",r"化肥，肥料","chemical,toxic,unnatural"],
+                        ["that",r"那；那个","this,it,they,those,these,such"],
+                        ["and",r"和，而且","or,as wellas ,both...and,not only...but also...,other than,in addition,besides,on the one hand ....on the other hand....neigher...nor..."],
+                        ["rather than",r"而非，不是","but,yet,however,whereas,nonetheless,neverthelss,notwithstanding,although,though,instead"],
+                        ["thanks to",r"由于，幸亏","stem from,derive,owingto , due to,according to ,because of , an accountof ,as a result of, leading to , because, since, for, in that, as, therefore,hence"]
+                        )
+                return words
+        def bookWordsSecond(slef):
+                words = (
+                        ["diversity",r"多样性，差异","variety,difference"],
+                        ["detect",r"查明，发现","find,look for, seek,scarch"]
+                )
+                return words
+
